576b76273285c7148a91de65f8a9a1550ea2cd08
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-12-01 17:51:39 +0600

    update snippets cache system

--- snippets/paginations.py
+++ snippets/paginations.py
@@ -2,4 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class SnippetPagination(PageNumberPagination):
     page_query_param = 'snippet_page'
-    page_size = 6
+    page_size = 8
--- snippets/signals.py
+++ snippets/signals.py
@@ -3,8 +3,29 @@ from django.core.cache import cache
 from django.dispatch import receiver
 from .models import Snippet
 
-@receiver(signal= [post_save, post_delete], sender= Snippet)
-def snippet_cache_delete(sender, instance, created, **kwarg):
+@receiver(signal= post_save, sender= Snippet)
+def snippets_cache_delete(sender, instance, created, **kwarg):
+
     if not created:
         cache_key = f'snippet_id:{instance.id}'
         cache.delete(cache_key)
+
+    total_pages = Snippet.objects.all().count() // 8
+    cache_page_key = f'snippets_list_page:{None}'
+    for i in range(1, total_pages + 2):
+        cache.delete(cache_page_key)
+        cache_page_key = f'snippets_list_page:{i}'
+
+@receiver(signal= post_delete, sender= Snippet)
+def snippets_cache_delete(sender, instance, **kwargs):
+
+    ## snippets all pagination delete from cache
+    total_pages = Snippet.objects.all().count() // 8
+    cache_page_key = f'snippets_list_page:{None}'
+    for i in range(1, total_pages + 2):
+        cache.delete(cache_page_key)
+        cache_page_key = f'snippets_list_page:{i}'
+
+    #specific snippet delete from cache
+    cache_key = f'snippet_id:{instance.id}'
+    cache.delete(cache_key)
